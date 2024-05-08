689fccb9cba77932603ed34c2588f0ced38d8428
Nuno <nno.ribeiro@gmail.com>
2024-05-08 19:15:23 -0400

    added static folder join to express

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const pinoLogger = require('./logger')
+const path = require('path')
 
 const connectToDatabase = require('./models/db')
 const { loadData } = require("./util/import-mongo/index")
