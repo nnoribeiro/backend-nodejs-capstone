04e62cbdca01546fc741b06e06ed1e240ca00ae8
Nuno <nno.ribeiro@gmail.com>
2024-05-08 14:15:41 -0400

    linting

--- sentiment/index.js
+++ sentiment/index.js
@@ -1,6 +1,5 @@
 require('dotenv').config()
 const express = require('express')
-const axios = require('axios')
 const logger = require('./logger')
 const expressPino = require('express-pino-logger')({ logger })
 // Task 1: import the natural library
@@ -16,7 +15,6 @@ app.use(expressPino)
 // Define the sentiment analysis route
 // Task 3: create the POST /sentiment analysis
 app.post('/sentiment', async (req, res) => {
-
   // Task 4: extract the sentence parameter
   const { sentence } = req.query
 
@@ -53,7 +51,7 @@ app.post('/sentiment', async (req, res) => {
   } catch (error) {
     logger.error(`Error performing sentiment analysis: ${error}`)
     // Task 7: if there is an error, return a HTTP code of 500 and the json {'message': 'Error performing sentiment analysis'}
-    res.status(500).json({ 'message': 'Error performing sentiment analysis' })
+    res.status(500).json({ message: 'Error performing sentiment analysis' })
   }
 })
 
